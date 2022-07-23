c900ae562d4ba7f03b568f5d6c9a32da44a1ef0d
julianv97 <julian.vicente@radiumrocket.com>
2022-07-23 02:07:03 -0300

    add font

---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Nunito Sans', sans-serif",
+    body: "'Body Font Name', sans-serif",
+  },
+});
+
+export default theme;
